f0490f1ede2af5682bf489fece51b5da1796bed8
Marcella Feliciano <153116960+MarcellaFeliciano@users.noreply.github.com>
2024-08-21 14:33:33 -0300

    Add files via upload

---
+++ aula_bd_flask/init_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# abre conexão
+conn = sqlite3.connect('database.db')
+
+# localização do sql
+SCHEMA = "database/schema.sql"
+
+# executa declarações do sql para o banco
+with open(SCHEMA) as f:
+    conn.executescript(f.read())
+
+# encerra operações
+conn.commit()
+conn.close()
+
+
+# CODIGO EXECUTADO UMA VEZ QUE IRÁ FAZER A CONEXãO DO BD
